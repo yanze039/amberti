c4c46ed777526ec0a3f1daf2fe53d0a332f91cce
yanze <yanze039@mit.edu>
2023-07-01 20:56:34 -0400

    add analysis

--- amberti/analysis.py
+++ amberti/analysis.py
@@ -9,33 +9,41 @@ import os
 from pathlib import Path
 import json
 
-base_dir = Path(os.getcwd())
-result = {}
-for cl in ["ligands", "complex"]:
-    wkdir = base_dir.joinpath(cl)
-    for job in ["decharge", "recharge", "vdw_bonded"]:
-        jobdir = wkdir.joinpath(job)
-        lmd_lists = jobdir.glob("[0-9]*.*[0-9]")
-        data_list = []
-        for lmd in lmd_lists:
-            data = extract_dHdl(lmd.joinpath("ti001.out"), T=300)
-            decorrelated_dhdl = decorrelate_dhdl(data, remove_burnin=True)
-            data_list.append(decorrelated_dhdl)
-        dHdl = alchemlyb.concat(data_list)
-        ti = TI()
-        ti.fit(dHdl)
-        fe = ti.delta_f_
-        result[f"{cl}-{job}"] = fe.loc[0.00, 1.00]
-        # R_c, running_average = fwdrev_cumavg_Rc(data_list, tol=2)
 
-        df = forward_backward_convergence(data_list, 'ti')
-        ax = plot_convergence(df)
-        ax.figure.savefig(f'pics/dF_t_{cl}_{job}.png')
+def analyze(workpath, output_dir):
+    base_dir = Path(workpath)
+    output_dir = Path(output_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    result = {}
+    for cl in ["ligands", "complex"]:
+        wkdir = base_dir.joinpath(cl)
+        for job in ["decharge", "recharge", "vdw_bonded"]:
+            jobdir = wkdir.joinpath(job)
+            lmd_lists = jobdir.glob("[0-9]*.*[0-9]")
+            data_list = []
+            for lmd in lmd_lists:
+                data = extract_dHdl(lmd.joinpath("prod.out"), T=300)
+                decorrelated_dhdl = decorrelate_dhdl(data, remove_burnin=True)
+                data_list.append(decorrelated_dhdl)
+            dHdl = alchemlyb.concat(data_list)
+            ti = TI()
+            ti.fit(dHdl)
+            fe = ti.delta_f_
+            result[f"{cl}-{job}"] = fe.loc[0.00, 1.00]
+            # R_c, running_average = fwdrev_cumavg_Rc(data_list, tol=2)
 
-with open('result.json', "w") as fp:
-    json.dump(result, fp, indent=4)
+            df = forward_backward_convergence(data_list, 'ti')
+            ax = plot_convergence(df)
+            ax.figure.savefig(output_dir.joinpath(f'dF_t_{cl}_{job}.png'))
 
-print("delta G = {}".format(
-    result["complex-vdw_bonded"] + result["complex-recharge"] + result["complex-decharge"] - \
-    (result["ligands-vdw_bonded"] + result["ligands-recharge"] + result["ligands-decharge"])
-))
+    ddG = result["complex-vdw_bonded"] + result["complex-recharge"] + result["complex-decharge"] - \
+        (result["ligands-vdw_bonded"] + result["ligands-recharge"] + result["ligands-decharge"])
+
+    result["ddG"] = ddG
+
+    with open(output_dir.joinpath('result.json'), "w") as fp:
+        json.dump(result, fp, indent=4)
+    
+    return result
+    
